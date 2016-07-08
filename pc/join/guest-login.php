0d972225dec6637f50e03f1d05dc745dd685a2fe
jyeon <jyeon@isecommerce.co.kr>
2016-07-08 18:29:26 +0900

    비회원 결제/주문완료

---
+++ pc/join/guest-login.php
@@ -0,0 +1,62 @@
+<?php include '../header/header.php'; ?>
+
+    <div class="content">
+        <div class="container">
+            <div class="login-banner">
+                <img src="../images/join/login-banner.jpg" alt="">
+            </div>
+        </div>
+        <div class="container">
+            <div class="row login">
+                <!-- 로그인 -->
+                <div class="col-xs-4">
+                    <div class="box">
+                        <h3 class="box-title">로그인</h3>
+                        <div class="form-group placeholder">
+                            <label for="id">아이디 (EMAIL)</label>
+                            <input type="text" id="id" class="form-control">
+                        </div>
+                        <div class="form-group placeholder">
+                            <label for="password">비밀번호</label>
+                            <input type="password" id="password" class="form-control">
+                        </div>
+                        <div class="login-find">
+                            <div class="checkbox">
+                                <input type="checkbox" id="save-id" checked="checked">
+                                <label for="save-id" class="checkbox-display"></label>
+                                <label for="save-id" class="checkbox-label">아이디 저장</label>
+                            </div>
+                            <a href="#" class="find-id">아이디/비밀번호 찾기</a>
+                        </div>
+                        <a href="#" class="button button-large button-primary button-block"><span>로그인</span></a>
+                    </div>
+                </div>
+                <!-- 로그인 //end -->
+                <!-- 회원가입 -->
+                <div class="col-xs-4">
+                    <div class="box">
+                        <h3 class="box-title">회원가입</h3>
+                        <p class="box-explain">
+                            Steven Alan 회원 가입을 진심으로 환영합니다.<br>
+                            만 14세 이상만 회원 가입이 가능하며, 회원이 되시면 다양한<br>
+                            혜택을 받으실 수 있고,<br>
+                            서비스를 안전하고 간편하게 이용 가능합니다.
+                        </p>
+                        <a href="#" class="button button-large button-primary button-block"><span>회원가입</span></a>
+                    </div>
+                </div>
+                <!-- 회원가입 //end -->
+                <!-- 비회원 주문 -->
+                <div class="col-xs-4">
+                    <div class="box">
+                        <h3 class="box-title">비회원 주문</h3>
+                        <p class="box-explain">비회원 주문 시 쿠폰 사용의 제한이 있으며 Steven Alan의<br>다양한 혜택을 받으실 수 없습니다.</p>
+                        <a href="#" class="button button-large button-primary button-block"><span>비회원 주문</span></a>
+                    </div>
+                </div>
+                <!-- 비회원 주문 //end -->
+            </div>
+        </div>
+    </div>
+
+<?php include '../footer/footer.php'; ?>
---
+++ pc/order/guest-summary.php
@@ -0,0 +1,146 @@
+<?php include '../header/header.php'; ?>
+
+    <div class="content">
+        <div class="container">
+            <ul class="order-process">
+                <li>shopping bag</li>
+                <li>checkout</li>
+                <li class="current">summary</li>
+            </ul>
+            <div class="box">
+                <h2 class="box-page-title">주문 완료/요약</h2>
+                <!-- 주문 완료 확인 -->
+                <div class="complete-box ordered-check">
+                    <span class="washed-line-title">thankyou</span>
+                    <strong class="confirm-message">홍길동 님의 주문이 확인되었습니다.</strong>
+                    <p>비회원 주문 현황은 <a href="#" class="link-line">로그인/회원가입&gt;비회원 주문조회</a>에서 확인하실 수 있습니다.</p>
+                    <ul class="ordered-info delimiter">
+                        <li>주문일자 2016-05-03</li>
+                        <li>주문번호 <strong class="ordered-num">Q00003744</strong></li>
+                    </ul>
+                </div>
+                <!-- 주문 완료 확인 //end -->
+                <!-- 주문 상품 정보 -->
+                <div class="summary">
+                    <h3 class="table-title">주문 상품 정보</h3>
+                    <table class="table table-list">
+                        <caption class="sr-only">비회원 주문 완료한 상품의 목록</caption>
+                        <colgroup>
+                            <col class="col-xs-9">
+                            <col class="col-xs-1">
+                            <col class="col-xs-1">
+                            <col class="col-xs-1">
+                        </colgroup>
+                        <thead>
+                        <tr>
+                            <th scope="col">상품정보</th>
+                            <th scope="col">수량</th>
+                            <th scope="col">상품금액</th>
+                            <th scope="col">주문금액</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td class="product-info aline-left">
+                                <div class="product-image"><img src="../images/temp/temp-product.jpg" alt=""></div>
+                                <div class="item-info">
+                                    <span class="item-badge">NEW</span>
+                                    <span class="item-brand">STEVEN ALAN</span>
+                                    <strong class="item-name">Belted Long Shirtdress</strong>
+                                    <ul class="item-option">
+                                        <li>Color : INDIGO</li>
+                                        <li>Size : P</li>
+                                    </ul>
+                                </div>
+                            </td>
+                            <td>1</td>
+                            <td>
+                                <em class="discount">298,000</em>
+                                <strong class="price">275,000</strong>
+                            </td>
+                            <td>
+                                <strong class="price">275,000</strong>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- 주문 상품 정보 //end -->
+                <!-- 결제 정보 -->
+                <div class="summary">
+                    <h3 class="table-title">주문 상품 정보</h3>
+                    <table class="table table-condensed">
+                        <caption class="sr-only">비회원 주문 상품 정보 목록</caption>
+                        <colgroup>
+                            <col class="col-xs-2">
+                            <col class="col-xs-2">
+                            <col class="col-xs-1">
+                            <col class="col-xs-7">
+                        </colgroup>
+                        <tbody>
+                        <tr>
+                            <th scope="row">총 상품 금액</th>
+                            <td colspan="3"><em class="benton">215,000</em>원</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">배송비</th>
+                            <td colspan="3"><em class="benton">9,000</em>원</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">총 결제 금액</th>
+                            <td><strong class="ordered-price"><em class="benton">255,000</em>원</strong></td>
+                            <th scope="row" class="border-left">가상계좌</th>
+                            <td>
+                                <ul class="virtual-account-info delimiter">
+                                    <li>국민은행 : <em class="benton">40849011172953</em></li>
+                                    <li>입금기한 : <strong><em class="benton">2016.06.30</em></strong></li>
+                                </ul>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- 결제 정보 //end -->
+                <!-- 배송지 정보 -->
+                <div class="summary">
+                    <h3 class="table-title">배송지 정보</h3>
+                    <table class="table table-condensed">
+                        <caption class="sr-only">비회원 배송지 정보 목록</caption>
+                        <colgroup>
+                            <col class="col-xs-2">
+                            <col class="col-xs-10">
+                        </colgroup>
+                        <tbody>
+                        <tr>
+                            <th scope="row">수령인</th>
+                            <td>김영희</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">연락처</th>
+                            <td><em class="benton">02-444-1234 / 010-1234-1234</em></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">배송지</th>
+                            <td><em class="benton">[04778]</em> 서울특별시 성동구 뚝섬로1길 25(성수동1가, 서울숲 한라에코밸리)  B105</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">배송 메세지</th>
+                            <td>부재시 경비실에 맡겨주세요.</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">개인통관고유부호</th>
+                            <td><em class="benton">M8510024485233</em></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- 배송지 정보 //end -->
+                <div class="button-align-center">
+                    <a href="#" class="button button-large button-primary">계속 쇼핑하기</a>
+                    <a href="#" class="button button-large">주문/배송 조회</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include '../footer/footer.php'; ?>
