b9457b43d76aed81da3aff22805bee7c4124216e
jyeon <jyeon@isecommerce.co.kr>
2016-07-20 18:02:25 +0900

    NEWS 화면

---
+++ pc/etc/news.php
@@ -0,0 +1,106 @@
+<?php include '../header/header.php'; ?>
+
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-2">
+                    <h2 class="page-title">news</h2>
+                </div>
+                <div class="col-xs-10">
+                    <div class="box news">
+                        <h3 class="box-page-title">NEWS</h3>
+                        <div class="news-contents">
+                            <p class="news-help-text">STEVEN ALAN의 새로운 소식을 알려드립니다.</p>
+                            <table class="table table-list folding-table">
+                                <caption></caption>
+                                <colgroup>
+                                    <col class="col-xs-2">
+                                    <col class="col-xs-8">
+                                    <col class="col-xs-2">
+                                </colgroup>
+                                <thead>
+                                <tr>
+                                    <th>구분</th>
+                                    <th>제목</th>
+                                    <th>작성일</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="master">
+                                    <td>이벤트</td>
+                                    <td class="align-left">신용카드 무이자 할부 이벤트 안내</td>
+                                    <td>2016-05-22</td>
+                                </tr>
+                                <tr class="slave">
+                                    <td colspan="3" class="news-content">
+                                        신용카드 무이자 할부 이벤트 안내<br>
+                                        신용카드 무이자 할부 이벤트 안내<br>
+                                        신용카드 무이자 할부 이벤트 안내<br>
+                                    </td>
+                                </tr>
+                                <tr class="master">
+                                    <td>공지</td>
+                                    <td class="align-left">하나카드 통합 전산시스템 작업에 따른 결제 서비스 일시 중지 안내</td>
+                                    <td>2016-05-22</td>
+                                </tr>
+                                <tr class="slave">
+                                    <td colspan="3" class="news-content">
+                                        안녕하세요, 고객님 Steven Alan 입니다.<br><br>
+                                        하나카드 통합 전산시스템 작업에 따른 결제 서비스 일시 중지 안내 드립니다.<br><br>
+                                        1.일시중지 서비스<br>
+                                        - ACS창을 이용하는 안심클릭, 삼성월렛, 이니페이 간편결제, 하나<br>
+                                        - KPAY 서비스<br><br>
+                                        2.작업 목적: 하나카드 통합 전산시스템 오픈 작업<br><br>
+                                        4.작업 시간<br>
+                                        - 안심클릭 등 신규가입 중단 : 7월 17일(금) 22시 ~ 7월 20일(월) 08시<br>
+                                        - 안심클릭 등 거래중단 : 7월20일(월) 00시 ~ 08시<br>
+                                        - Kpay 서비스 거래중단 : 7월20일(월) 01시 ~ 05시<br><br>
+                                        하나카드 통합 전산시스템 정비작업 시간 동안 일부 결제 서비스가 일시 중지 될 예정이오니 서비스 이용에 착오 없으시기 바랍니다.<br>
+                                        감사합니다.
+                                    </td>
+                                </tr>
+                                <tr class="master">
+                                    <td>공지</td>
+                                    <td class="align-left">약관 변경 및 추가 안내</td>
+                                    <td>2016-05-22</td>
+                                </tr>
+                                <tr class="slave">
+                                    <td colspan="3" class="news-content">
+                                        약관 변경 및 추가 안내<br>
+                                        약관 변경 및 추가 안내<br>
+                                        약관 변경 및 추가 안내
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <!-- paging -->
+                            <div class="paging">
+                                <a href="#" class="prev disabled"><i class="icon-left"></i></a>
+                                    <span class="pagingNum">
+                                        <span class="current"><a href="#">1</a></span>
+                                        <span><a href="#">2</a></span>
+                                        <span><a href="#">3</a></span>
+                                        <span><a href="#">4</a></span>
+                                    </span>
+                                <a href="#" class="next"><i class="icon-right"></i></a>
+                            </div>
+                            <!-- paging //end -->
+                        </div>
+                        <div class="button-align-center search-word-box">
+                            <div class="input-button-group">
+                                <div class="form-group form-inline">
+                                    <div class="placeholder">
+                                        <label for="search_word">검색어 입력</label>
+                                        <input type="text" id="search_word" class="form-control">
+                                    </div>
+                                    <div><a href="#" class="button">검색</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include '../footer/footer.php'; ?>
